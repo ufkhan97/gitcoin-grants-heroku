623bc2c7e209b01d0e8131e3fe6ed7cd3ac3cf80
Pulkit <70895323+realguy33@users.noreply.github.com>
2023-12-01 07:19:48 +0530

    Update and rename donorList.py to DonorList.py

---
+++ pages/DonorList.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import plotly.express as px
+import utils 
+
+st.set_page_config(
+    page_title="Data - Gitcoin Leaderboard",
+    page_icon="favicon.png",
+    layout="wide",
+)
+
+st.title('💸 Projects and their donors')
+
+st.write("This page gives you the list of donors for a particular project after you enter the project id.The projectid should have the same capitalization as is given in gitcoin")
+
+
+program_data = pd.read_csv("all_rounds.csv")
+program_option = st.selectbox( 'Select Program', program_data['program'].unique())
+st.title(program_option)
+
+if "program_option" in st.session_state and st.session_state.program_option != program_option:
+    st.session_state.data_loaded = False
+st.session_state.program_option = program_option
+
+if "data_loaded" in st.session_state and st.session_state.data_loaded:
+    dfv = st.session_state.dfv
+    dfp = st.session_state.dfp
+    round_data = st.session_state.round_data
+else:
+    data_load_state = st.text('Loading data...')
+    dfv, dfp, round_data = utils.load_round_data(program_option, "all_rounds.csv")
+    data_load_state.text("")
+
+
+
