b821175f5732d7c7f4ca928c54fa4a767e194c39
Pulkit <70895323+realguy33@users.noreply.github.com>
2023-12-01 07:26:59 +0530

    Update DonorList.py

--- pages/DonorList.py
+++ pages/DonorList.py
@@ -20,29 +20,3 @@ st.write("test - test")
 project_id = st.text_input('Enter Project ID', 'none')
 
 
-program_data = pd.read_csv("all_rounds.csv")
-program_option = st.selectbox( 'Select Program', program_data['program'].unique())
-st.title(program_option)
-
-
-
-if "program_option" in st.session_state and st.session_state.program_option != program_option:
-    st.session_state.data_loaded = False
-st.session_state.program_option = program_option
-
-if "data_loaded" in st.session_state and st.session_state.data_loaded:
-    dfv = st.session_state.dfv
-    dfp = st.session_state.dfp
-    round_data = st.session_state.round_data
-else:
-    data_load_state = st.text('Loading data...')
-    dfv, dfp, round_data = utils.load_round_data(program_option, "all_rounds.csv")
-    data_load_state.text("")
-
-if(project_id != 'none'):
-    st.write("Loading - may take upto 2 minutes")
-    filtered_data = dfv[dfv['projectId'] == project_id]
-    required_columns = filtered_data[['voter_id', 'block_timestamp', 'token_symbol','amount','amountUSD', 'round_name']]
-    st.dataframe(required_columns)
-
-
