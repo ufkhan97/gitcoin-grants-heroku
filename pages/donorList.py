0444730842cc1871b60181ba418e2ea9f7cd44b6
Pulkit <70895323+realguy33@users.noreply.github.com>
2023-12-01 07:12:23 +0530

    Create donorList.py

---
+++ pages/donorList.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import plotly.express as px
+import utils 
+
+st.set_page_config(
+    page_title="Data - Gitcoin Leaderboard",
+    page_icon="favicon.png",
+    layout="wide",
+)
