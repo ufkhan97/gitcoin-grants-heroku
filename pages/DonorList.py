7b9917f69161faaeba732b880305b8ec89a0485b
Pulkit <70895323+realguy33@users.noreply.github.com>
2023-12-01 07:24:59 +0530

    Update DonorList.py

--- pages/DonorList.py
+++ pages/DonorList.py
@@ -15,6 +15,8 @@ st.title('💸 Projects and their donors')
 
 st.write("This page gives you the list of donors for a particular project after you enter the project id.The projectid should have the same capitalization as is given in gitcoin")
 
+st.write("test - test")
+
 project_id = st.text_input('Enter Project ID', 'none')
 
 
