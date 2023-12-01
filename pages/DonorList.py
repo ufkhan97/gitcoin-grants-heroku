d459b1f3d7052f6d713ba6dc6bdf6009803839dd
Pulkit <70895323+realguy33@users.noreply.github.com>
2023-12-01 07:24:10 +0530

    Update DonorList.py

--- pages/DonorList.py
+++ pages/DonorList.py
@@ -15,12 +15,13 @@ st.title('💸 Projects and their donors')
 
 st.write("This page gives you the list of donors for a particular project after you enter the project id.The projectid should have the same capitalization as is given in gitcoin")
 
+project_id = st.text_input('Enter Project ID', 'none')
+
 
 program_data = pd.read_csv("all_rounds.csv")
 program_option = st.selectbox( 'Select Program', program_data['program'].unique())
 st.title(program_option)
 
-project_id = st.text_input('Enter Project ID', 'none')
 
 
 if "program_option" in st.session_state and st.session_state.program_option != program_option:
