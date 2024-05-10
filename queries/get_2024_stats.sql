1fa9e7c15a51694f6cf7df3d5d394e03e1bdff79
Umar Khan <ufkhan97@gmail.com>
2024-05-09 19:48:53 -0600

    push lifetime stats

--- queries/get_2024_stats.sql
+++ queries/get_2024_stats.sql
@@ -2,8 +2,11 @@ SELECT
   SUM(r."total_amount_donated_in_usd") as crowdfunded_usd,
   SUM(CASE WHEN r."matching_distribution" IS NOT NULL THEN r."match_amount_in_usd" ELSE 0 END) AS matchingfunds,
   count(r."matching_distribution") as num_rounds,
-  sum(r."total_donations_count") as num_donations,
+  sum(r."total_donations_count") as num_donations
   
+  --count(distinct r."donor_address") as unique_voters,
+  --count(distinct r."recipient_address") as unique_grantees
+
 FROM
   "chain_data_63"."rounds" AS r
 WHERE 
