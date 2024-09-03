3dee69dc2532fce1bf5e87915e3f44fe83cae3e2
Umar Khan <ufkhan97@gmail.com>
2024-09-02 17:56:00 -0700

    Update get_2024_stats.sql

--- queries/get_2024_stats.sql
+++ queries/get_2024_stats.sql
@@ -3,7 +3,6 @@ SELECT
   SUM(CASE WHEN r."matching_distribution" IS NOT NULL THEN r."match_amount_in_usd" ELSE 0 END) AS matchingfunds,
   count(r."matching_distribution") as num_rounds,
   sum(r."total_donations_count") as num_donations
-  
   --count(distinct r."donor_address") as unique_voters,
   --count(distinct r."recipient_address") as unique_grantees
 FROM
